f5a45e47144fb46e69cc287ef666b2dda4207c3e
CRISTIAN <cjgc4002@gmail.com>
2025-05-11 11:23:18 -0300

    Feat: adiciona pattern DDD

---
+++ src/main/java/br/com/frontier/rh_simplificado/shared/AggregateRoot.java
@@ -0,0 +1,12 @@
+package br.com.frontier.rh_simplificado.shared;
+
+/**
+ * @author Cristian Gluchak <cjgc4002@gmail.com>
+ * @since 10/05/2025
+ */
+public abstract class AggregateRoot<ID extends Identifier> extends Entity<ID> {
+
+    public AggregateRoot(ID id){
+        super(id);
+    }
+}
---
+++ src/main/java/br/com/frontier/rh_simplificado/shared/ValueObject.java
@@ -0,0 +1,8 @@
+package br.com.frontier.rh_simplificado.shared;
+
+/**
+ * @author Cristian Gluchak <cjgc4002@gmail.com>
+ * @since 10/05/2025
+ */
+public class ValueObject {
+}
