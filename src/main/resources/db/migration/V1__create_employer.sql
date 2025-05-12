97c341837661651c9e1d17115388dcb7aac9cd7e
CRISTIAN <cjgc4002@gmail.com>
2025-05-11 22:40:25 -0300

    Feat: implementa migration do empregador

---
+++ src/main/resources/db/migration/V1__create_employer.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS employer(
+        id_employer  SERIAL         NOT NULL,
+        fantasyname  VARCHAR(255)  NOT NULL,
+        razaosocial  VARCHAR(255)  NOT NULL,
+        cnpj         VARCHAR(14)  NOT NULL,
+        email        VARCHAR(255)  NOT NULL,
+       CONSTRAINT pk_employer PRIMARY KEY (id_employer)
+);
