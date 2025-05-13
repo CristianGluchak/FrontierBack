1876b8b59a0d0787d362258bdfd9eae97d44c863
CRISTIAN <cjgc4002@gmail.com>
2025-05-12 23:19:48 -0300

    Feat: inicia implementacao de update

---
+++ src/main/java/br/com/frontier/rh_simplificado/employer/application/update/UpdateEmployerUseCase.java
@@ -0,0 +1,8 @@
+package br.com.frontier.rh_simplificado.employer.application.update;
+
+/**
+ * @author Cristian Gluchak <cjgc4002@gmail.com>
+ * @since 12/05/2025
+ */
+public class UpdateEmployerUseCase {
+}
