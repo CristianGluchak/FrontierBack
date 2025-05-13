e3544ec90be1e65ff5fdda11d4148c99c35dd36f
CRISTIAN <cjgc4002@gmail.com>
2025-05-12 23:22:49 -0300

    Feat: inicia Resource para endpoints

---
+++ src/main/java/br/com/frontier/rh_simplificado/employer/infrastructure/rest/EmployerResource.java
@@ -0,0 +1,9 @@
+package br.com.frontier.rh_simplificado.employer.infrastructure.rest;
+
+/**
+ * @author Cristian Gluchak <cjgc4002@gmail.com>
+ * @since 12/05/2025
+ */
+public class EmployerResource {
+
+}
