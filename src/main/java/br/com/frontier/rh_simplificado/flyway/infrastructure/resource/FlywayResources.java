342bc1f0fbfd408f6a9cc5f812ac53e9dfb6d2b6
CRISTIAN <cjgc4002@gmail.com>
2025-05-18 23:49:26 -0300

    Feat: implementa processo para passar a rodar migrations via endpoint

---
+++ src/main/java/br/com/frontier/rh_simplificado/flyway/infrastructure/resource/FlywayResources.java
@@ -0,0 +1,25 @@
+package br.com.frontier.rh_simplificado.flyway.infrastructure.resource;
+
+import br.com.frontier.rh_simplificado.flyway.infrastructure.queries.FlywayMigrationUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Cristian Gluchak <cjgc4002@gmail.com>
+ * @since 18/05/2025
+ */
+@RestController
+@RequestMapping("/admin/rh")
+@RequiredArgsConstructor
+public class FlywayResources {
+
+    private final FlywayMigrationUseCase flywayMigrationUseCase;
+
+    @PostMapping("/migrate")
+    public String migrate(){
+        flywayMigrationUseCase.migrate();
+        return "Migração executada com sucesso!";
+    }
+}
