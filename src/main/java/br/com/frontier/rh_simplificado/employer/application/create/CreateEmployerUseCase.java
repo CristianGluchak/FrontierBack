02d645b09dfbef9aa8aba25c0f082bbb330c12bd
CRISTIAN <cjgc4002@gmail.com>
2025-05-18 23:43:30 -0300

    Feat: implementa retorno para caso de uso

--- src/main/java/br/com/frontier/rh_simplificado/employer/application/create/CreateEmployerUseCase.java
+++ src/main/java/br/com/frontier/rh_simplificado/employer/application/create/CreateEmployerUseCase.java
@@ -2,22 +2,24 @@ package br.com.frontier.rh_simplificado.employer.application.create;
 
 import br.com.frontier.rh_simplificado.employer.domain.commands.CreateEmployerCommand;
 import br.com.frontier.rh_simplificado.employer.domain.entities.Employer;
+import br.com.frontier.rh_simplificado.employer.domain.entities.EmployerID;
 import br.com.frontier.rh_simplificado.employer.infrastructure.persistence.entities.EmployerJpaEntity;
 import br.com.frontier.rh_simplificado.employer.infrastructure.persistence.repositories.EmployerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Cristian Gluchak <cjgc4002@gmail.com>
  * @since 12/05/2025
  */
 
+@Service
 @RequiredArgsConstructor
 public class CreateEmployerUseCase {
 
     private final EmployerRepository repository;
 
-
-    public void execute(CreateEmployerInput input){
+    public EmployerID execute(CreateEmployerInput input){
 
         CreateEmployerCommand command = CreateEmployerCommand.builder()
                 .razaoSocial(input.getRazaoSocial())
@@ -29,6 +31,6 @@ public class CreateEmployerUseCase {
         Employer employer = Employer.create(command);
 
         repository.save(EmployerJpaEntity.from(employer));
-
+        return employer.getId();
     }
 }
