159b3af52d813e2c6f062de65304aafad039cedd
CRISTIAN <cjgc4002@gmail.com>
2025-07-06 22:44:21 -0300

    Feat: inicia implementacao da camada de aplicacao

---
+++ src/main/java/br/com/frontier/rh_simplificado/salarySheet/application/create/CreatePayrollUseCase.java
@@ -0,0 +1,15 @@
+package br.com.frontier.rh_simplificado.salarySheet.application.create;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Cristian Gluchak <cjgc4002@gmail.com>
+ * @since 06/07/2025
+ */
+@Service
+@RequiredArgsConstructor
+public class CreatePayrollUseCase   {
+
+
+}
