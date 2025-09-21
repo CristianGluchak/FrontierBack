0c46c413f3b8c4e73872e8aba18c01ee1e87e1a1
Cristian Gluchak <cristian.gluchak@nexuscloud.com.br>
2025-09-21 17:06:52 -0300

    feat: implementa caso de uso de calculo de folha

--- src/main/java/br/com/frontier/rh_simplificado/payroll/application/calculate/CreatePayrollUseCase.java
+++ src/main/java/br/com/frontier/rh_simplificado/payroll/application/calculate/CreatePayrollUseCase.java
@@ -4,9 +4,14 @@ import br.com.frontier.rh_simplificado.employee.domain.entities.Employee;
 import br.com.frontier.rh_simplificado.employee.infrastructure.persistence.entities.EmployeeJpaEntity;
 import br.com.frontier.rh_simplificado.employee.infrastructure.persistence.repositories.EmployeeRepository;
 import br.com.frontier.rh_simplificado.payroll.domain.commands.CreatePayrollCommand;
+import br.com.frontier.rh_simplificado.payroll.domain.commands.UpdatePayrollCommand;
 import br.com.frontier.rh_simplificado.payroll.domain.entities.Payroll;
+import br.com.frontier.rh_simplificado.payroll.infrastructure.persistence.entities.PayrollJpaEntity;
+import br.com.frontier.rh_simplificado.payroll.infrastructure.persistence.repositories.PayrollRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.YearMonth;
 
 import static java.lang.String.format;
 
@@ -20,6 +25,8 @@ public class CreatePayrollUseCase {
 
     private final EmployeeRepository employeeRepository;
 
+    private final PayrollRepository payrollRepository;
+
     public void execute(final CreatePayrollInput input) {
         Employee employee = employeeRepository.findById(input.getEmployeeID().getValue())
             .map(EmployeeJpaEntity::ToDomain)
@@ -27,21 +34,34 @@ public class CreatePayrollUseCase {
                 "Não foi possivel localizar um funcionario com o id %s",
                 input.getEmployeeID().getValue())));
 
-        /*TODO: implementar consulta por mes para validar calculo
-        * folha ja existente no mes então substitua pela calculada
-        *
-         */
-        CreatePayrollCommand command = CreatePayrollCommand.builder()
-            .employeeID(input.getEmployeeID())
-            .employerID(input.getEmployerID())
-            .baseSalary(employee.getSalary())
-            .build();
+        payrollRepository.findByEmployerAndEmployeeAndReferenceMonth(input.getEmployerID()
+                .getValue(),
+            input.getEmployeeID().getValue(),
+            YearMonth.now()).ifPresentOrElse(
+            payrollJpaEntity -> {
 
-        Payroll payroll = Payroll.create(command);
+                Payroll payrollDomain = payrollJpaEntity.toDomain();
 
+                UpdatePayrollCommand command = UpdatePayrollCommand.builder()
+                    .employeeID(input.getEmployeeID())
+                    .employerID(input.getEmployerID())
+                    .baseSalary(employee.getSalary())
+                    .build();
+                payrollDomain.update(command);
 
+                payrollRepository.save(PayrollJpaEntity.from(payrollDomain));
+            },
+            () -> {
+
+                CreatePayrollCommand command = CreatePayrollCommand.builder()
+                    .employeeID(input.getEmployeeID())
+                    .employerID(input.getEmployerID())
+                    .baseSalary(employee.getSalary())
+                    .build();
+
+                Payroll payroll = Payroll.create(command);
+                payrollRepository.save(PayrollJpaEntity.from(payroll));
+            });
 
     }
-
-
 }
