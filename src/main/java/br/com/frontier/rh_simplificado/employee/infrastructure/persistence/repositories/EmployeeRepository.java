660ea673ecf42871b128e5dbd2a71ef61d7a396b
Cristian Gluchak <cristian.gluchak@nexuscloud.com.br>
2025-09-23 22:22:39 -0300

    Feat: implementa consulta para buscar por empresa e situação

--- src/main/java/br/com/frontier/rh_simplificado/employee/infrastructure/persistence/repositories/EmployeeRepository.java
+++ src/main/java/br/com/frontier/rh_simplificado/employee/infrastructure/persistence/repositories/EmployeeRepository.java
@@ -1,8 +1,10 @@
 package br.com.frontier.rh_simplificado.employee.infrastructure.persistence.repositories;
 
 import br.com.frontier.rh_simplificado.employee.infrastructure.persistence.entities.EmployeeJpaEntity;
+import br.com.frontier.rh_simplificado.shared.enums.AtivoInativo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -10,4 +12,6 @@ import java.util.UUID;
  * @since 12/05/2025
  */
 public interface EmployeeRepository extends JpaRepository<EmployeeJpaEntity, UUID> {
+
+    List<EmployeeJpaEntity> findByEmployerIdAndStatus(UUID employerId, AtivoInativo status);
 }
