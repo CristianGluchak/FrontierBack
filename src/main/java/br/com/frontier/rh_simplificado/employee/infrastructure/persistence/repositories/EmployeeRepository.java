279c8c9d3dbf6afacc2d3be0e27849926d059a6b
Cristian Gluchak <cristian.gluchak@nexuscloud.com.br>
2025-10-06 21:55:48 -0300

    feat: implementa recurso de listagem utilizando reposytory

--- src/main/java/br/com/frontier/rh_simplificado/employee/infrastructure/persistence/repositories/EmployeeRepository.java
+++ src/main/java/br/com/frontier/rh_simplificado/employee/infrastructure/persistence/repositories/EmployeeRepository.java
@@ -3,6 +3,7 @@ package br.com.frontier.rh_simplificado.employee.infrastructure.persistence.repo
 import br.com.frontier.rh_simplificado.employee.infrastructure.persistence.entities.EmployeeJpaEntity;
 import br.com.frontier.rh_simplificado.shared.enums.AtivoInativo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,7 +12,8 @@ import java.util.UUID;
  * @author Cristian Gluchak <cjgc4002@gmail.com>
  * @since 12/05/2025
  */
-public interface EmployeeRepository extends JpaRepository<EmployeeJpaEntity, UUID> {
+public interface EmployeeRepository extends JpaRepository<EmployeeJpaEntity, UUID>,
+    JpaSpecificationExecutor<EmployeeJpaEntity> {
 
     List<EmployeeJpaEntity> findByEmployerIdAndStatus(UUID employerId, AtivoInativo status);
 }
